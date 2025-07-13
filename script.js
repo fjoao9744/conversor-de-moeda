bc2615a85848a9d6404491fe7a26f2299cc5ffb9
fjoao9744 <fjoao9744@gmail.com>
2025-07-13 12:45:42 -0300

    add coins

--- script.js
+++ script.js
@@ -10,23 +10,31 @@ const getInput = () => {
 
 document.getElementById("coin1__input").addEventListener("input", async () => {;
     let [coin1, coin2] = getInput();
+    try {
+        let conversion = await fetch(`https://economia.awesomeapi.com.br/json/last/${coin1}-${coin2}`)
+        .then(res => res.json()).then(res => res[coin1 + coin2]);
+    
+        const price = (parseFloat(conversion.ask) + parseFloat(conversion.bid)) / 2;
+    
+        coin2__value.value = (coin1__value.value * price).toFixed(5);
 
-    let conversion = await fetch(`https://economia.awesomeapi.com.br/json/last/${coin1}-${coin2}`)
-    .then(res => res.json()).then(res => res[coin1 + coin2]);
-
-    const price = (parseFloat(conversion.ask) + parseFloat(conversion.bid)) / 2;
-
-    coin2__value.value = (coin1__value.value * price).toFixed(5);
+    } catch {
+        coin2__value.value = "Conversion invalid.";
+    }
 })
 
 document.getElementById("coin2__input").addEventListener("input", async () => {;
     let [coin1, coin2] = getInput();
+    try {
+        let conversion = await fetch(`https://economia.awesomeapi.com.br/json/last/${coin2}-${coin1}`)
+        .then(res => res.json()).then(res => res[coin2 + coin1]);
+    
+        const price = (parseFloat(conversion.ask) + parseFloat(conversion.bid)) / 2;
+    
+        coin1__value.value = (coin2__value.value * price).toFixed(5);
 
-    let conversion = await fetch(`https://economia.awesomeapi.com.br/json/last/${coin2}-${coin1}`)
-    .then(res => res.json()).then(res => res[coin2 + coin1]);
-
-    const price = (parseFloat(conversion.ask) + parseFloat(conversion.bid)) / 2;
-
-    coin1__value.value = (coin2__value.value * price).toFixed(5);
+    } catch {
+        coin2__value.value = "Conversion invalid.";
+    }
 })
 
