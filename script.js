3a3b4a20d23b8a8c4e332fbf10a8af2d437f846b
fjoao9744 <fjoao9744@gmail.com>
2025-07-13 12:41:13 -0300

    api

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ document.getElementById("coin1__input").addEventListener("input", async () => {;
 
     const price = (parseFloat(conversion.ask) + parseFloat(conversion.bid)) / 2;
 
-    coin2__value.value = coin1__value.value * price;
+    coin2__value.value = (coin1__value.value * price).toFixed(5);
 })
 
 document.getElementById("coin2__input").addEventListener("input", async () => {;
     let [coin1, coin2] = getInput();
 
-    let conversion = await fetch(`https://economia.awesomeapi.com.br/json/last/${coin1}-${coin2}`)
-    .then(res => res.json()).then(res => res[coin1 + coin2]);
+    let conversion = await fetch(`https://economia.awesomeapi.com.br/json/last/${coin2}-${coin1}`)
+    .then(res => res.json()).then(res => res[coin2 + coin1]);
 
     const price = (parseFloat(conversion.ask) + parseFloat(conversion.bid)) / 2;
 
-    coin1__value.value = coin2__value.value * price;
+    coin1__value.value = (coin2__value.value * price).toFixed(5);
 })
 
